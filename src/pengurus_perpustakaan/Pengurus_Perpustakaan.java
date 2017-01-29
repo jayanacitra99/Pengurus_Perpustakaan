917d7f302a842fdf0ef1d007b30b534bdf799584
Jayana Citra <jayana_putra_24rpl@student.smktelkom-mlg.sch.id>
2017-01-29 22:22:59 +0700

    First Commit

---
+++ src/pengurus_perpustakaan/Pengurus_Perpustakaan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pengurus_perpustakaan;
+
+/**
+ *
+ * @author Jayana Citra
+ */
+public class Pengurus_Perpustakaan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
